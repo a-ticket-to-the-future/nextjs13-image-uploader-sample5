f31017b59766531fa5e3ca4acfe08414da196b28
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-11-21 14:16:34 +0900

    gitの順番間違えた

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -6,12 +6,20 @@ import {getStorage} from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdyqGxWMmNF4AmVjFB-XwHpM9ubGVUT58",
-  authDomain: "nextjs13-imageuploader-sample4.firebaseapp.com",
-  projectId: "nextjs13-imageuploader-sample4",
-  storageBucket: "nextjs13-imageuploader-sample4.appspot.com",
-  messagingSenderId: "480176505176",
-  appId: "1:480176505176:web:dfbaf77874ddc7ffe3e52d"
+
+    // apiKey: "AIzaSyCdyqGxWMmNF4AmVjFB-XwHpM9ubGVUT58",
+    authDomain: "nextjs13-imageuploader-sample4.firebaseapp.com",
+    projectId: "nextjs13-imageuploader-sample4",
+    storageBucket: "nextjs13-imageuploader-sample4.appspot.com",
+    messagingSenderId: "480176505176",
+    appId: "1:480176505176:web:dfbaf77874ddc7ffe3e52d",
+    
+    apiKey: process.env.FIREBASE_API_KEY,
+    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
